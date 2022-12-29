04ede12b58a7c526f32059b67ca4a75ec95d82f0
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2022-12-29 00:26:03 -0600

    Time: 4 ms (77.75%), Space: 40.8 MB (70.81%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -0,0 +1,27 @@
+
+    class Solution {
+    public String smallestSubsequence(String s) {
+        Stack<Character> stack=new Stack<>();
+        HashSet<Character> seen=new HashSet<>();
+        HashMap<Character, Integer> map=new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            map.put(s.charAt(i), i);
+        }
+        for(int i=0; i<s.length(); i++){
+            char c=s.charAt(i);
+            if(!seen.contains(c)){
+                while(!stack.isEmpty() && c < stack.peek() && map.get(stack.peek()) > i){
+                    seen.remove(stack.pop());
+                }
+                seen.add(c);
+                stack.push(c);
+            }
+        }
+        StringBuilder sb=new StringBuilder(stack.size());
+        for(Character c: stack){
+            sb.append(c.charValue());
+            
+        }
+        return sb.toString(); 
+    }
+}
