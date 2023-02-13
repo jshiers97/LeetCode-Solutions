e3f3b4274b494df71c8e954a29c3907d7033a424
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-02-12 21:20:44 -0600

    Time: 320 ms (9.13%), Space: 44.2 MB (23.21%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,25 +1,93 @@
 class Solution {
-    int m;
+    List<List<Integer>> output=new ArrayList<>(); 
+    int[][] DIRECTIONS=new int[][]{ {0, 1}, {1,0}, {-1, 0}, {0, -1}};
+    int m; 
     int n;
     int[][] heights;
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-       this.heights=heights;
-        List<List<Integer>> output=new ArrayList<>();
         this.m=heights.length;
         this.n=heights[0].length;
-        boolean[][] Atlantic=new boolean[m][n];
-        boolean[][] Pacific=new boolean[m][n];
+        this.heights=heights;
+       boolean[][] Atlantic=new boolean[m][n];
+        boolean[][] Pacific = new boolean[m][n];
+        Queue<int[]> q=new LinkedList<>(); //Pacific 
+        Queue<int[]> q2=new LinkedList<>();
         for(int i=0; i<m; i++){
-            dfs(i, 0, Pacific, -1);
-            dfs(i, n-1, Atlantic, -1);
+            Pacific[i][0]=true;
+            q.offer(new int[]{i, 0});
+            Atlantic[i][n-1]=true;
+            q2.offer(new int[]{i, n-1});
         }
         for(int i=0; i<n; i++){
-            dfs(0, i, Pacific, -1);
-            dfs(m-1, i, Atlantic, -1);
+            Pacific[0][i]=true;
+            q.offer(new int[]{0, i});
+            Atlantic[m-1][i]=true;
+            q2.offer(new int[]{m-1, i});
         }
-        for(int i=0; i<m;i++){
+        //boolean[][] temp=bfs(q);
+        //boolean[][] temp2=bfs(q2);
+        boolean [][] visited=new boolean[m][n];
+        for(boolean[] row: visited){
+            Arrays.fill(row, false); 
+        }
+        while(!q.isEmpty()){
+            int[] curr=q.poll(); 
+            int x=curr[0];
+            int y=curr[1];
+            visited[x][y]=true;
+            Pacific[x][y]=true;
+            for(int[] dir: DIRECTIONS){
+                int r=x+dir[0];
+                int c=y+dir[1];
+                if(r < 0 || r >= m || c < 0 || c>=n || visited[r][c] ){
+                    continue;
+                }
+                else if(heights[r][c] < heights[x][y]){
+                    continue;
+                }
+                else{
+                    q.offer(new int[]{r, c});
+                    
+                }
+            }
+            
+        }
+        for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(Atlantic[i][j] && Pacific[i][j]){
+                System.out.println(i +" and " + j + "  is " + Atlantic[i][j]);
+            }
+        }
+        visited=new boolean[m][n];
+        for(boolean[] row: visited){
+            Arrays.fill(row, false); 
+        }
+        while(!q2.isEmpty()){
+            int[] curr=q2.poll(); 
+            int x=curr[0];
+            int y=curr[1];
+            visited[x][y]=true;
+            Atlantic[x][y]=true;
+            for(int[] dir: DIRECTIONS){
+                int r=x+dir[0];
+                int c=y+dir[1];
+                if(r < 0 || r >= m || c < 0 || c>=n || visited[r][c] ){
+                    continue;
+                }
+                else if(heights[r][c] < heights[x][y]){
+                    continue;
+                }
+                else{
+                    q2.offer(new int[]{r, c});
+                    
+                }
+            }
+            
+        }
+      
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                
+                if(Pacific[i][j] && Atlantic[i][j]){
                     List<Integer> insert=new ArrayList<>();
                     insert.add(i);
                     insert.add(j);
@@ -32,16 +100,32 @@ class Solution {
         
         
     }
-    
-    public void dfs(int i, int j, boolean[][] seen, int prevValue){
-        if(i>=m || i<0 || j>=n || j<0 || heights[i][j]<prevValue || seen[i][j]){
-            return;
-        }
-        seen[i][j]=true;
-        dfs(i+1, j, seen, heights[i][j]);
-        dfs(i-1, j, seen, heights[i][j]);
-        dfs(i, j+1, seen, heights[i][j]);
-        dfs(i, j-1, seen, heights[i][j]);
-        return;
-    }
+    // private boolean[][] bfs(Queue<int[]> queue) {
+    //     boolean[][] reachable = new boolean[m][n];
+    //     while (!queue.isEmpty()) {
+    //         int[] cell = queue.poll();
+    //         // This cell is reachable, so mark it
+    //         reachable[cell[0]][cell[1]] = true;
+    //         for (int[] dir : DIRECTIONS) { // Check all 4 directions
+    //             int newRow = cell[0] + dir[0];
+    //             int newCol = cell[1] + dir[1];
+    //             // Check if new cell is within bounds
+    //             if (newRow < 0 || newRow >= m || newCol < 0 || newCol >= n) {
+    //                 continue;
+    //             }
+    //             // Check that the new cell hasn't already been visited
+    //             if (reachable[newRow][newCol]) {
+    //                 continue;
+    //             }
+    //             // Check that the new cell has a higher or equal height,
+    //             // So that water can flow from the new cell to the old cell
+    //             if (heights[newRow][newCol] < heights[cell[0]][cell[1]]) {
+    //                 continue;
+    //             }
+    //             // If we've gotten this far, that means the new cell is reachable
+    //             queue.offer(new int[]{newRow, newCol});
+    //         }
+    //     }
+    //     return reachable;
+    // }
 }
