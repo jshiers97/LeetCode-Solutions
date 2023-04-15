6c6d1bf73deb236fe1093710daa31ccb2d0c09b0
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-04-15 16:02:55 -0500

    Time: 13 ms (21.05%), Space: 50.1 MB (39.47%) - LeetHub

---
+++ 1131-maximum-of-absolute-value-expression/1131-maximum-of-absolute-value-expression.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxAbsValExpr(int[] arr1, int[] arr2) {
+        int max1=Integer.MIN_VALUE;
+        int max2=Integer.MIN_VALUE;
+        int max3=Integer.MIN_VALUE;
+        int max4=Integer.MIN_VALUE;
+        int min1=Integer.MAX_VALUE;
+        int min2=Integer.MAX_VALUE;
+        int min3=Integer.MAX_VALUE;
+        int min4=Integer.MAX_VALUE;
+        for(int i=0; i<arr1.length; i++){
+            max1=Math.max(max1, arr1[i] + arr2[i] + i);
+            min1=Math.min(min1, arr1[i] + arr2[i] + i);
+            max2=Math.max(max2, arr1[i] - arr2[i] + i);
+            min2=Math.min(min2, arr1[i] - arr2[i] + i);
+            max3=Math.max(max3, arr1[i] - arr2[i] - i);
+            min3=Math.min(min3, arr1[i] - arr2[i] - i);
+            max4=Math.max(max4, -arr1[i] - arr2[i] + i);
+            min4=Math.min(min4, -arr1[i] - arr2[i] + i);
+        }
+        return Math.max(max1-min1, Math.max(max2-min2, Math.max(max3-min3, max4-min4)));
+    }
+}
