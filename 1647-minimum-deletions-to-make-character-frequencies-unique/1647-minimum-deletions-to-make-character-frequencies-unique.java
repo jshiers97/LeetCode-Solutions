b73fc9c871fdb3a5972d5cdf8df068bb449c198b
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-04-30 18:05:35 -0500

    Time: 13 ms (44.10%), Space: 43.3 MB (62.68%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,41 +1,24 @@
 class Solution {
     public int minDeletions(String s) {
-        //p<Character, Integer> count=new HashMap<>(); 
-        int[] chars=new int[26];
-        for(int i=0; i<s.length(); i++){
-            char c=s.charAt(i); 
-            chars[c-'a']++;
+        int[] chars = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            chars[s.charAt(i) - 'a']++;
         }
-        PriorityQueue<Integer> pq=new PriorityQueue<>( (x, y) -> (y-x)); 
+        Set<Integer> freq=new HashSet<>();
+        int total=0; 
         for(int i=0; i<26; i++){
-            if(chars[i]>0){
-                pq.offer(chars[i]);
-            }
+            
+                while(chars[i]>0 && freq.contains(chars[i])){
+                    chars[i]--;
+                    total++; 
+                }
+
+                freq.add(chars[i]);
             
         }
-        int deletes=0; 
-        while(!pq.isEmpty()){
-            int curr=pq.poll(); 
-            //System.out.println(curr); 
-            if(pq.isEmpty()){
-                continue;
-            }
-            else{
-                int next=pq.peek(); 
-                if(curr==next){
-                    curr--;
-                    deletes++;
-                    if(curr>0){
-                        //System.out.println("here" + curr);
-                        pq.offer(curr); 
-                    }
-                    
-                }
-                
-            }
-        }
-        return deletes; 
-        
+        return total; 
         
     }
+     
+        
 }
