68fe9f40023a75d10068f1b66844fad94f0f8170
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-04-30 01:56:14 -0500

    Time: 12 ms (37.90%), Space: 49.9 MB (46.77%) - LeetHub

--- 1131-maximum-of-absolute-value-expression/1131-maximum-of-absolute-value-expression.java
+++ 1131-maximum-of-absolute-value-expression/1131-maximum-of-absolute-value-expression.java
@@ -13,10 +13,10 @@ class Solution {
             min1=Math.min(min1, arr1[i] + arr2[i] + i);
             max2=Math.max(max2, arr1[i] - arr2[i] + i);
             min2=Math.min(min2, arr1[i] - arr2[i] + i);
-            max3=Math.max(max3, arr1[i] - arr2[i] - i);
-            min3=Math.min(min3, arr1[i] - arr2[i] - i); 
-            max4=Math.max(max4, -arr1[i] - arr2[i] + i);
-            min4=Math.min(min4, -arr1[i] - arr2[i] + i);
+            max3=Math.max(max3, arr1[i] + arr2[i] - i);
+            min3=Math.min(min3, arr1[i] + arr2[i] - i); 
+            max4=Math.max(max4, arr1[i] - arr2[i] - i);
+            min4=Math.min(min4, arr1[i] - arr2[i] - i);
         }
         return Math.max(max1-min1, Math.max(max2-min2, Math.max(max3-min3, max4-min4)));
     }
