da148f5af1d966e866fcd140c8efb11fea793307
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-05-02 12:16:04 -0500

    Time: 2020 ms (5.00%), Space: 43.1 MB (13.72%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,96 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        if(num1.equals("0") || num2.equals("0")){
+            return "0";
+            }
+        List<Integer> output=new ArrayList<>(); 
+        output.add(0); 
+       
+        int zeroes=0;
+        
+        for(int i2=num2.length()-1; i2>=0; i2--){
+            List<Integer> curr=new ArrayList<>(); 
+            int carry=0;
+            for(int i=0; i<zeroes; i++){
+                    curr.add(0, 0); 
+            }
+            for(int i1=num1.length()-1; i1>=0; i1--){
+                
+                int first=num1.charAt(i1)-'0'; 
+                int second=num2.charAt(i2)-'0'; 
+                int prod=(first*second) + carry;
+                
+                carry=prod/10;
+                prod%=10;
+                System.out.println(prod);
+                curr.add(0, prod); 
+            }
+            if(carry>0){
+                System.out.println("HEREEE");
+                curr.add(0, carry); 
+            }
+            
+            
+            for(int i=0; i<curr.size(); i++){
+                System.out.print(curr.get(i)); 
+                
+            }
+            System.out.println("next1");
+            output=add(curr, output); 
+            curr=new ArrayList<>(); 
+            
+            
+            zeroes++;
+            for(int i=0; i<output.size(); i++){
+                System.out.print(output.get(i)); 
+                
+            }
+            System.out.println("next");
+        }
+        StringBuilder out=new StringBuilder(); 
+        for(int i=0; i<output.size();i++){
+            out.append(output.get(i));
+        }
+        return out.toString(); 
+        
+        
+        
+    }
+    public List add(List<Integer> first, List<Integer> second){
+        int carry=0;
+        List<Integer> res=new ArrayList<>();
+        int i1=first.size()-1;
+        int i2=second.size()-1;
+        while(i1>=0 && i2>=0){
+            int num1=first.get(i1);
+            int num2=second.get(i2);
+            
+            int sum=(num1+num2+carry);
+            carry=(sum)/10;
+            sum%=10;
+            res.add(0, sum); 
+            i1--;
+            i2--;
+        }
+        while(i1>=0){
+            int num1=first.get(i1);
+            int sum=num1+carry;
+            carry=sum/10;
+            sum%=10;
+            res.add(0, sum);
+            i1--;
+        }
+        while(i2>=0){
+            int num2=second.get(i2);
+            int sum=num2+carry;
+            carry=sum/10;
+            sum%=10;
+            res.add(0, sum);
+            i2--;
+        }
+        if(carry>0){
+            res.add(0, carry); 
+        }
+        return res; 
+    }
+}
