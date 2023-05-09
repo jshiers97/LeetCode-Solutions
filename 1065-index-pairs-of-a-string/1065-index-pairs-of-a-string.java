fe972f2674cfc1105c7b487830ff6a2ef5b19cde
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-05-09 12:01:42 -0500

    Time: 6 ms (43.38%), Space: 43.1 MB (42.72%) - LeetHub

--- 1065-index-pairs-of-a-string/1065-index-pairs-of-a-string.java
+++ 1065-index-pairs-of-a-string/1065-index-pairs-of-a-string.java
@@ -1,23 +1,48 @@
 class Solution {
+    
+    class TrieNode{
+        public boolean flag;
+        public Map<Character, TrieNode> next=new HashMap<>(); 
+    }
+    
+    class Trie{
+        public TrieNode root;
+        public Trie(){
+            root=new TrieNode(); 
+        }
+        public void insert(String words){
+            TrieNode curr=root; 
+            for(int i=0; i<words.length(); i++){
+                char c=words.charAt(i); 
+                if(curr.next.get(c)==null){
+                    curr.next.put(c, new TrieNode());
+                }
+                curr=curr.next.get(c);
+                
+            }
+            curr.flag=true;
+        }
+    }
     public int[][] indexPairs(String text, String[] words) {
-        List<int[]> out=new ArrayList<>(); 
-        
-        Set<String> set=new HashSet<>();
+        List<int[]> list=new ArrayList<>(); 
+        Trie root=new Trie(); 
         for(String str: words){
-            set.add(str); 
+            root.insert(str); 
         }
         for(int i=0; i<text.length(); i++){
+            TrieNode curr=root.root; 
             for(int j=i; j<text.length(); j++){
-                String sub=text.substring(i, j+1);
-                if(set.contains(sub)){
-                    out.add(new int[]{ i, j});
+                if(curr.next.get(text.charAt(j))==null){
+                    break;
+                }
+                curr=curr.next.get(text.charAt(j)); 
+                if(curr.flag){
+                    list.add(new int[]{i, j});
                 }
             }
         }
-        int[][] output=new int[out.size()][2];
-        for(int i=0; i<out.size(); i++){
-            output[i]=out.get(i); 
-        }
-        return output; 
+        int[][] ans=new int[list.size()][2];
+        ans=list.toArray(ans); 
+        return ans;
     }
 }
