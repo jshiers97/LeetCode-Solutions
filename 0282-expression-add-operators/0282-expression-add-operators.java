844f9427b1a2896f66bdf7b175313886ed06c662
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-05-09 15:11:17 -0500

    Time: 144 ms (11.18%), Space: 43.6 MB (36.02%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.java
+++ 0282-expression-add-operators/0282-expression-add-operators.java
@@ -13,32 +13,32 @@ class Solution {
     }
     public void backtrack(int index, long currVal, long prevVal, String curr){
         if(index==num.length() && currVal==target){
-            output.add(curr);
+            output.add(new String(curr));
             return;
         }
-        if(index > num.length()){
+        if(index>num.length()){
             return;
         }
         for(int i=index; i<num.length(); i++){
+            long val;
             if(i>index && num.charAt(index)=='0'){
                 continue;
             }
-            
-            long val=Long.valueOf(num.substring(index, i+1));
-            
+            else{
+                val=Long.valueOf(num.substring(index, i+1));
+            }
             if(curr.length()==0){
                 backtrack(i+1, val, val, val + "");
             }
             else{
-                String add=curr + "+" + val;
-                backtrack(i+1, currVal + val, val, add );
-                String sub=curr + "-" + val;
-                backtrack(i+1, currVal - val, -val, sub);
+                String add=curr+ "+" + val;
+                backtrack(i+1, currVal + val, val, add);
+                String del=curr + "-" + val;
+                backtrack(i+1, currVal-val, -val, del);
                 String mult=curr + "*" + val;
-                long multVal=(currVal-prevVal) + (prevVal * val);
-                backtrack(i+1, multVal, prevVal *val, mult);
+                backtrack(i+1, (currVal-prevVal) + (prevVal * val), prevVal*val, mult);
             }
+             
         }
-        return;
     }
 }
