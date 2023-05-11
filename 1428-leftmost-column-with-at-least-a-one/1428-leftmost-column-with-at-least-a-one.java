6694b0572abeb9297502de8e7727c62159c20b16
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-05-11 18:10:28 -0500

    Time: 10 ms (8.99%), Space: 43.5 MB (22.47%) - LeetHub

---
+++ 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.java
@@ -0,0 +1,48 @@
+/**
+ * // This is the BinaryMatrix's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface BinaryMatrix {
+ *     public int get(int row, int col) {}
+ *     public List<Integer> dimensions {}
+ * };
+ */
+
+class Solution {
+    public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
+        int m=binaryMatrix.dimensions().get(0); 
+        int n=binaryMatrix.dimensions().get(1); 
+        
+        
+        List<Integer> rs=new ArrayList<>();
+        for(int i=0; i<m; i++){
+            if(binaryMatrix.get(i, n-1)==1){
+                rs.add(i);
+            }
+        }
+        if(rs.size()==0){
+            return -1; 
+        }
+       
+        int right=n-1; 
+        int min=n-1; 
+        for(int x: rs){
+            int left=0;
+            right=min;
+            System.out.println(x + "rowa"); 
+            while(left <= right){
+                int mid=(left+right)/2;
+                if(binaryMatrix.get(x, mid)==1){
+                    System.out.println(mid);
+                    min=Math.min(mid, min); 
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1; 
+                }
+            }
+            
+        }
+        
+        return min;
+    }
+}
