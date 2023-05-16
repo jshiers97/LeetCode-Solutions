8106745b9199b57d3c4b7487ab6e8ce477ef056a
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-05-16 15:19:56 -0500

    Time: 1690 ms (5.01%), Space: 58 MB (72.50%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        if(connections.length < n-1){
+            return -1; 
+        }
+        int[] root=new int[n];
+        for(int i=0; i<n; i++){
+            root[i]=i;
+        }
+        int change=0;
+        for(int[] connection: connections){
+            int x=connection[0];
+            int y=connection[1];
+            if(find(root, x)!=find(root, y)){
+                union(x, y, root);
+            }
+            else{
+                change++; 
+            }
+            
+        }
+        Set<Integer> unique=new HashSet<>();
+        //System.out.println(first); 
+        int temp=0;
+        for(int i=0; i<root.length; i++){
+            System.out.println(root[i]);
+            if(!unique.contains(root[i])){
+                unique.add(root[i]);
+                temp++;
+            }
+        }
+        return Math.min(change, temp-1);
+    }
+    public void union(int x, int y, int[] root){
+        int findx=find(root, x);
+        int findy=find(root, y);
+        if(findx!=findy){
+            for(int i=0; i<root.length; i++){
+                if(root[i]==findy){
+                    root[i]=findx;
+                }
+            }
+        }
+    }
+    public int find(int[] root, int find){
+        return root[find];
+    }
+}
