5d8140a1090bb18c485231ff208488a565da056f
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-05-15 20:24:23 -0500

    Time: 361 ms (8.27%), Space: 109.4 MB (10.74%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,40 +1,31 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
         List<List<Integer>> output=new ArrayList<>();
-        Set<Integer> champs=new HashSet<>();
-        Set<Integer> runner=new HashSet<>();
-        Set<Integer> losers=new HashSet<>();
+        output.add(new ArrayList<>());
+        output.add(new ArrayList<>()); 
+        int[] players=new int[1000002];
+        Arrays.fill(players, -1);
         for(int[] match: matches){
             int winner=match[0];
             int loser=match[1];
-            if(champs.contains(loser)){
-                champs.remove(loser);
-                runner.add(loser);
+            if(players[winner]==-1){
+                players[winner]=0;
             }
-            else if(runner.contains(loser)){
-                runner.remove(loser);
-                losers.add(loser); 
-            }
-            else if(!losers.contains(loser)){
-                runner.add(loser);
+            if(players[loser]==-1){
+                players[loser]=1;
             }
             else{
-                losers.add(loser);
-            }
-            if(!losers.contains(winner) && !runner.contains(winner)){
-                champs.add(winner); 
+                players[loser]++;
             }
         }
-        output.add(new ArrayList<>());
-        output.add(new ArrayList<>()); 
-        for(Integer x: champs){
-            output.get(0).add(x); 
+        for(int i=0; i<players.length; i++){
+            if(players[i]==0){
+                output.get(0).add(i);
+            }
+            else if(players[i]==1){
+                output.get(1).add(i); 
+            }
         }
-        for(Integer x: runner){
-            output.get(1).add(x); 
-        }
-        Collections.sort(output.get(0));
-        Collections.sort(output.get(1));
         return output; 
     }
 }
