dc690625bb103a4a2eacf9bbd280b9a452f76470
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-05-16 21:07:15 -0500

    Time: 436 ms (31.37%), Space: 51 MB (8.66%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,49 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int left=0;
         Arrays.sort(nums); 
         List<List<Integer>> output=new ArrayList<>(); 
-        while(left<nums.length-2){
-            
-            int index=left+1; 
-            int right=nums.length-1; 
-            
-            while(index<right){
-                int curr=nums[left] + nums[index] + nums[right];
-                
-                if(curr==0){
-                    List<Integer> insert=new ArrayList<>(); 
-                    insert.add(nums[left]);
-                    insert.add(nums[index]); 
-                    insert.add(nums[right]); 
-                    output.add(insert); 
-                    index++;
-                    while(index < right && nums[index]==nums[index-1]){
-                        index++;
-                    }
-                    right--; 
-                    while(right > index && nums[right]==nums[right+1]){
-                        right--;
-                    }
-                }
-                else if(curr>0){
-                    
-                    right--;
-                }
-                else{
-                    
-                    index++; 
-                }
-            }
-           
-            left++;
-            while(left < nums.length && nums[left]==nums[left-1] && nums[left]<=0){
-                left++;
-            }
-            if(left==nums.length || nums[left]>0){
+        for(int i=0; i<nums.length-2; i++){
+            if(nums[i] > 0){
                 return output; 
+            }
+            else if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            else{
+                twoSum(nums, i, output); 
             }
         }
         return output; 
+    }
+    public void twoSum(int[] nums, int i, List<List<Integer>> output){
+        Set<Integer> set=new HashSet<>(); 
+        int target=0 - nums[i];
+        for(int j=i+1; j<nums.length; j++){
+            int complement=-nums[j]-nums[i];
+            if(set.contains(complement)){
+                List<Integer> insert=new ArrayList<>();
+                insert.add(nums[i]);
+                insert.add(complement);
+                insert.add(nums[j]);
+                output.add(insert); 
+                while(j+1 < nums.length && nums[j]==nums[j+1]){
+                    j++; 
+                }
+            }
+            set.add(nums[j]);
+            
+        }
     }
 }
