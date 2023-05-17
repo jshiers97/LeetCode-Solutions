c898bb14608e65f4f2d6bc6c08da33784e46e4de
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-05-17 18:39:41 -0500

    Time: 467 ms (5.09%), Space: 43.7 MB (19.25%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,53 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if(s.equals(t)){
+            return s; 
+        }
+        if(s.length() < t.length()){
+            return "";
+        }
+        int[] needed=new int[128];
+        int needCount=0;
+        for(char c: t.toCharArray()){
+            needed[c-'A']++;
+            if(needed[c-'A']==1){
+                needCount++;
+            }
+        }
+        int left=0;
+        int[] have=new int[128];
+        int right=0; 
+        int min=Integer.MAX_VALUE;
+        int haveCount=0; 
+        String res="";
+        while(right < s.length()){
+            char c=s.charAt(right); 
+            
+            have[c-'A']++;
+            if(have[c-'A']==needed[c-'A']){
+                haveCount++;
+            }
+            System.out.println(right + "right is");
+            while(haveCount==needCount){
+               
+                
+                if(min > right-left+1){
+                   
+                    min=right-left+1;
+                    res=s.substring(left, right+1); 
+                }
+             
+                char d=s.charAt(left++);
+                have[d-'A']--;
+                if(have[d-'A']+1 == needed[d-'A'] && needed[d-'A']>0){
+                    haveCount--; 
+                }
+                
+            }
+            right++; 
+            
+        }
+        return res; 
+        
+    }
+}
