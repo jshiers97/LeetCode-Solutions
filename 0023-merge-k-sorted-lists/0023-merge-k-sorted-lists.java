d480a2fde75f24eb61000ccbe1af8c658cd3816f
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-05-18 22:54:50 -0500

    Time: 1 ms (100.00%), Space: 43.8 MB (93.24%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,48 +10,41 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length==0 || lists==null){
+        
+        if(lists.length==0){
             return null; 
         }
-        if(lists.length==1){
-            return lists[0]; 
+        int interval=1;
+        while(interval < lists.length){
+            for(int i=0; i+interval < lists.length; i=i+interval*2){
+                lists[i]=mergeTwoLists(lists[i], lists[i+interval]);
+            }
+            interval*=2;
         }
-       
-        ListNode sentinel=new ListNode(-1);
-        ListNode head=sentinel;
-        int min=100001; 
-        int minIndex=-1;
-        int empty=0;
-        while(empty<lists.length){
-            for(int i=0; i<lists.length; i++){
-                ListNode curr=lists[i];
-                if(curr==null){
-                    continue;
-                }
-                if(curr.val < min){
-                    min=curr.val;
-                    minIndex=i; 
-                }
-            }
-            ListNode insert=new ListNode(min); 
-            if(min==100001){
-                return sentinel.next;
-            }
-            
-            head.next=insert;
-            head=head.next;
-            ListNode shift=lists[minIndex];
-            if(shift.next==null){
-                empty++;
-                
-            }
-            shift=shift.next;
-            lists[minIndex]=shift;
-           
-            min=100001;
-            minIndex=-1;
-        }
-        return sentinel.next;
+        return lists[0];
         
+    }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        ListNode h=new ListNode(0);
+        ListNode ans=h;
+        while(l1!=null && l2!=null){
+            if(l1.val < l2.val){
+                h.next=l1;
+                h=h.next;
+                l1=l1.next;
+            }
+            else{
+                h.next=l2;
+                h=h.next;
+                l2=l2.next;
+            }
+        }
+        if(l1==null){
+            h.next=l2;
+        }
+        if(l2==null){
+            h.next=l1;
+        }
+        return ans.next;
     }
 }
