ebd8826b803a7febebe9d403b2dabc2f9cc9656f
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-05-21 14:41:25 -0500

    Time: 6 ms (8.63%), Space: 40.4 MB (20.65%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length==1){
+            return nums[0];
+        }
+        if(nums.length==2){
+            return Math.max(nums[0], nums[1]);
+        }
+        int[] dp1=new int[nums.length-1];
+        int[] dp2=new int[nums.length-1];
+        int[] nums1=new int[nums.length-1];
+        int[] nums2=new int[nums.length-1];
+        for(int i=0; i<nums.length-1; i++){
+            nums1[i]=nums[i];
+        }
+        for(int i=1; i<nums.length; i++){
+            nums2[i-1]=nums[i];
+        }
+        dp1[0]=nums[0];
+        dp1[1]=nums[1];
+        dp2[0]=nums[1];
+        dp2[1]=nums[2];
+        int max1=rob1(nums1, dp1);
+        for(int x: nums2){
+            System.out.println(x); 
+        }
+        //System.out.println(max1); 
+        int max2=rob1(nums2, dp2);
+        return Math.max(max1, max2); 
+    }
+    
+    
+    public int rob1(int[] nums, int[] dp) {
+        
+        
+        if(nums.length ==1){
+            return nums[0];
+        }
+        
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0], nums[1]);
+        for(int i=2; i<nums.length; i++){
+            int take=dp[i-1];
+            int skip=dp[i-2] + nums[i];
+            dp[i]=Math.max(take, skip); 
+        }
+       // System.out.println(dp[nums.length-1]); 
+        return Math.max(dp[nums.length-1], dp[nums.length-2]);
+    }
+}
