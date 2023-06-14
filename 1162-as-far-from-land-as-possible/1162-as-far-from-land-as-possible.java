a0991946b0d064b3e5b29f061c40e1e73eca382a
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-06-13 19:57:50 -0500

    Time: 496 ms (5.32%), Space: 44.6 MB (16.91%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int maxDistance(int[][] grid) {
+        int max=-1;
+        int m=grid.length;
+        int n=grid[0].length;
+        int[][] DIRECTIONS=new int[][]{ {0,1}, {1, 0}, {0, -1}, {-1, 0}};
+        Queue<int[]> q=new LinkedList<>();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==1){
+                    grid[i][j]=-1;
+                    q.offer(new int[]{i, j});
+                }
+                else{
+                    //grid[i][j]=Integer.MAX_VALUE;
+                }
+            }
+        }
+        
+        while(!q.isEmpty()){
+            int[] curr=q.poll(); 
+            int x=curr[0];
+            int y=curr[1];
+            int dist; 
+            if(grid[x][y]==-1){
+                dist=1;
+            }
+            else{
+                dist=grid[x][y]+1;
+            }
+            for(int[] dir: DIRECTIONS){
+                int r=x+dir[0];
+                int c=y+dir[1];
+                if(r<0 || r>= m || c<0 || c>=n || grid[r][c]==-1){
+                    continue; 
+                }
+                if(grid[r][c]==0){
+                    grid[r][c]=dist;
+                    max=Math.max(max, dist);
+                    q.offer(new int[]{r, c});
+                }
+                else if(grid[r][c] > dist){
+                    System.out.println("HERE");
+                    grid[r][c]=dist;
+                    q.offer(new int[]{r, c});
+                    max=Math.max(max, dist);
+                }
+                
+            }
+        }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                System.out.println(grid[i][j]);
+            }
+        }
+        
+        
+        
+        return max; 
+    }
+}
