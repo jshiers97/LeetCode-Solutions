5bf1330f30f348f143793c870b84e0675bf9938f
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-07-02 00:16:19 -0500

    Time: 1 ms (21.18%), Space: 41.2 MB (19.70%) - LeetHub

---
+++ 0158-read-n-characters-given-read4-ii-call-multiple-times/0158-read-n-characters-given-read4-ii-call-multiple-times.java
@@ -0,0 +1,44 @@
+/**
+ * The read4 API is defined in the parent class Reader4.
+ *     int read4(char[] buf4); 
+ */
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    Queue<Character> q=new LinkedList<>(); 
+ 
+    int curr4=0; 
+    int index=0; 
+    public int read(char[] buf, int n) {
+        int output=0;
+        
+        if(n==0){
+            return 0; 
+        }
+        char[] intBuff = new char[4];
+        
+        while (q.size() < n) {
+            int readCount = read4(intBuff);
+            for (int i = 0; i < readCount; i++) {
+                q.offer(intBuff[i]);
+            }
+            if (readCount < 4) break;
+        }
+        for(int i=0; i<n ; i++){
+            if(q.size() ==0){
+                break;
+            }
+            //output++;
+            buf[output++]=q.poll(); 
+        }
+        return output; 
+        
+       
+    }
+        
+        
+}
