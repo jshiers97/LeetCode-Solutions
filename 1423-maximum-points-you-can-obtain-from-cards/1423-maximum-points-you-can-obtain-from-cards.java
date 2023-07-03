33d7cde337e7dbbb23f24e39988ba9f8c6a3983b
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-07-03 11:47:15 -0500

    Time: 2 ms (95.04%), Space: 55.7 MB (47.47%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -10,24 +10,23 @@ class Solution {
         int answer=0; 
         int left=0; 
         int right=cardPoints.length-k-1; 
-        System.out.println(right); 
+       
         int currSum=0; 
         for(int i=0; i<=right; i++){
             currSum+=cardPoints[i];
         }
-        System.out.println(currSum); 
-        
+      
         
        
         answer=totalSum-currSum; 
         currSum-=cardPoints[right];
-        System.out.println(answer); 
+        
         while(right < cardPoints.length){
             currSum+=cardPoints[right];
             answer=Math.max(answer, totalSum-currSum); 
             currSum-=cardPoints[left++];
             right++; 
-            System.out.println(totalSum-currSum); 
+           
             
             
         }
