96f4a32ebeb9aa494015d1b7807e54dbe6a83510
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-07-23 23:14:49 -0500

    Time: 2 ms (83.23%), Space: 41.8 MB (53.29%) - LeetHub

--- 0439-ternary-expression-parser/0439-ternary-expression-parser.java
+++ 0439-ternary-expression-parser/0439-ternary-expression-parser.java
@@ -2,7 +2,7 @@ class Solution {
     public String parseTernary(String expression) {
         StringBuilder str=new StringBuilder();
         str.append(expression); 
-        System.out.println(str);
+        
         while(str.length() > 1){
             int question=0;
             int colon=0;
@@ -14,21 +14,21 @@ class Solution {
                 else if(c==':'){
                     colon++;
                 }
-                System.out.println(i);
+               
                 if(question==colon){
                     String temp;
                     if(str.charAt(0)=='T'){
                         temp=str.substring(2, i);
-                        System.out.println(temp + "Here");
+                        
                     }
                     else{
                         temp=str.substring(i+1);
-                        System.out.println(temp);
+                       
                     }
                     str=new StringBuilder();
                     str.append(temp);
                     break;
-                    //System.out.println(str);
+                   
                 }
             }
         }
