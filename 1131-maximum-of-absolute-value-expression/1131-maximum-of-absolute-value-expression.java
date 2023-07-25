b26d565d3f6282286048475a23079aa7ac18067e
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-07-24 22:03:30 -0500

    Time: 12 ms (58.71%), Space: 52.6 MB (23.87%) - LeetHub

--- 1131-maximum-of-absolute-value-expression/1131-maximum-of-absolute-value-expression.java
+++ 1131-maximum-of-absolute-value-expression/1131-maximum-of-absolute-value-expression.java
@@ -9,15 +9,19 @@ class Solution {
         int min3=Integer.MAX_VALUE;
         int min4=Integer.MAX_VALUE;
         for(int i=0; i<arr1.length; i++){
-            max1=Math.max(max1, arr1[i] + arr2[i] + i);
-            min1=Math.min(min1, arr1[i] + arr2[i] + i);
-            max2=Math.max(max2, arr1[i] - arr2[i] + i);
-            min2=Math.min(min2, arr1[i] - arr2[i] + i);
-            max3=Math.max(max3, arr1[i] + arr2[i] - i);
-            min3=Math.min(min3, arr1[i] + arr2[i] - i); 
-            max4=Math.max(max4, arr1[i] - arr2[i] - i);
-            min4=Math.min(min4, arr1[i] - arr2[i] - i);
+            max1=Math.max(arr1[i] + arr2[i] + i, max1);
+            max2=Math.max(arr1[i] - arr2[i] + i, max2);
+            max3=Math.max(arr1[i] + arr2[i] - i, max3);
+            max4=Math.max(arr1[i] - arr2[i] - i, max4);
+            min1=Math.min(arr1[i] + arr2[i] + i, min1);
+            min2=Math.min(arr1[i] - arr2[i] + i, min2);
+            min3=Math.min(arr1[i] + arr2[i] - i, min3);
+            min4=Math.min(arr1[i] - arr2[i] - i, min4);
+            
         }
-        return Math.max(max1-min1, Math.max(max2-min2, Math.max(max3-min3, max4-min4)));
+        int maxVal=Math.max(max1-min1, max2-min2);
+        int maxVal2=Math.max(max3-min3, max4-min4);
+        return Math.max(maxVal, maxVal2);
+        
     }
 }
