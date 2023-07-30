8064cbb608c574eef8ac03daefc118975cb96718
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-07-29 22:58:44 -0500

    Time: 26 ms (94.99%), Space: 43.6 MB (63.55%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.java
+++ 0935-knight-dialer/0935-knight-dialer.java
@@ -1,24 +1,23 @@
 class Solution {
     Integer[][] combos=new Integer[][]{{4, 6}, {6, 8}, {7, 9}, {4, 8}, {3, 9, 0},
                                         {},{1, 7, 0}, {2, 6}, {1, 3}, {2, 4}};
-    int MOD = (int)1e9 + 7;
     public int knightDialer(int n) {
-        int[][] dp=new int[n+1][10];
-        for(int j=0; j<10; j++){
-            dp[1][j]=1;
+        int[][] memo=new int[n+1][10];
+        for(int i=0; i<=9; i++){
+            memo[1][i]=1;
         }
         for(int i=2; i<=n; i++){
-            for(int j=0; j<10; j++){
+            for(int j=0; j<=9; j++){
                 for(int neighbor: combos[j]){
-                    dp[i][j]+=dp[i-1][neighbor];
-                    dp[i][j] %=MOD;
+                    memo[i][j]+=memo[i-1][neighbor];
+                    memo[i][j]%=1000000007;
                 }
             }
         }
         int answer=0;
         for(int i=0; i<10; i++){
-            answer+= dp[n][i];
-            answer%=MOD;
+            answer+= memo[n][i];
+            answer%=1000000007;
         }
         return answer;
     }
