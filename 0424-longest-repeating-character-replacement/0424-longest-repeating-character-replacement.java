7c24b91bb2d2d5b57271abecc7c98137d822a98d
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-09-25 19:05:06 -0500

    Time: 20 ms (33.43%), Space: 43.5 MB (26.54%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,25 +1,25 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         int left=0;
-        int[] frequencyMap=new int[26];
-        int maxFrequency=0;
-        int longestSubstringLength=0; 
-        for(int end=0; end<s.length(); end++){
-            int currChar=s.charAt(end)-'A';
-            frequencyMap[currChar]++;
-            maxFrequency=Math.max(maxFrequency, frequencyMap[currChar]);
-            boolean isValid=(end+1 - left - maxFrequency<= k);
-            if(!isValid){
-                int outgoingChar=s.charAt(left)-'A'; 
-                frequencyMap[outgoingChar] -= 1;
-
-                // move the start pointer forward
-                left += 1;
+        int right=0;
+        int longest=0; 
+        int res=0; 
+        Map<Character, Integer> map=new HashMap<>(); 
+        while(right < s.length()){
+            char c=s.charAt(right);
+            int val=map.getOrDefault(c, 0);
+            map.put(c, val+1); 
+            longest=Math.max(longest, map.get(c)); 
+            
+            while((right-left +1 ) - longest > k){
+                int temp=map.get(s.charAt(left));
+                map.put(s.charAt(left), temp-1);
+                left++; 
             }
-            longestSubstringLength=end+1-left; 
-                
+            res=Math.max(res, right-left+1); 
+            right++; 
             
         }
-        return longestSubstringLength;
+        return res; 
     }
 }
