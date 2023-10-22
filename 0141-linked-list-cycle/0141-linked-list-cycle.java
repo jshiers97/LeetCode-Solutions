e292132487f3fab3f40a0fe51a970f3458049500
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-10-22 00:39:24 -0500

    Time: 0 ms (100.00%), Space: 43.4 MB (55.00%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,18 +11,24 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head==null || head.next==null || head.next.next==null){
+        if(head==null || head.next==null){
             return false;
         }
-        ListNode fast=head.next;
         ListNode slow=head;
-        while(fast.next!=null && fast.next.next!=null){
-            fast=fast.next.next;
+        ListNode fast=head;
+        
+        while(fast!=null && fast.next!=null){
             slow=slow.next;
+            fast=fast.next.next;
             if(fast==slow){
-                return true;
+                break;
             }
         }
+        
+        if(slow==fast){
+            return true;  
+        }
         return false;
+        
     }
 }
