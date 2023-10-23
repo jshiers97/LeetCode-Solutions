95141d484a5c205297a18e432bade7858626d392
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-10-23 11:31:52 -0500

    Time: 128 ms (9.59%), Space: 43.1 MB (65.70%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,41 +10,36 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+        ListNode sent=new ListNode(-1);
+        ListNode ret=sent;
+        int max=Integer.MAX_VALUE;
+        int index=-1;
         
-        if(lists.length==0){
-            return null; 
-        }
-        int interval=1;
-        while(interval < lists.length){
-            for(int i=0; i+interval < lists.length; i=i+interval*2){
-                lists[i]=mergeTwoLists(lists[i], lists[i+interval]);
+        
+        while(true){
+            for(int i=0; i<lists.length; i++){
+                if(lists[i]==null){
+                    continue;
+                }
+                if(lists[i].val < max){
+                    max=lists[i].val;
+                    index=i; 
+
+                }
             }
-            interval*=2;
-        }
-        return lists[0];
-        
-    }
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
-        ListNode h=new ListNode(0);
-        ListNode ans=h;
-        while(l1!=null && l2!=null){
-            if(l1.val < l2.val){
-                h.next=l1;
-                h=h.next;
-                l1=l1.next;
+            if(index==-1){
+                break;
             }
             else{
-                h.next=l2;
-                h=h.next;
-                l2=l2.next;
+                ListNode insert=new ListNode(max);
+                sent.next=insert;
+                sent=insert;
+                lists[index]=lists[index].next;
+                index=-1;
+                max=Integer.MAX_VALUE;
             }
         }
-        if(l1==null){
-            h.next=l2;
-        }
-        if(l2==null){
-            h.next=l1;
-        }
-        return ans.next;
+        return ret.next;
+        
     }
 }
