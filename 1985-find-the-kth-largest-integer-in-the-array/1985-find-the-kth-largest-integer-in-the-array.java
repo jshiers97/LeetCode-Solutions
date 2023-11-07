9fb3a7ab862bac4e40c4d5af81bd718015e8f92d
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-11-06 20:11:19 -0600

    Time: 45 ms (74.60%), Space: 50 MB (60.29%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.java
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.java
@@ -3,19 +3,7 @@ class Solution {
         PriorityQueue<String> maxHeap=new PriorityQueue<>(new Comparator<String>(){
             public int compare(String one, String two){
                 if(one.length()==two.length()){
-                    // for(int i=0; i<two.length(); i++){
-                    //     // char c=one.charAt(i);
-                    //     // char d=two.charAt(i);
-                    //     // if(c>d){
-                    //     //     return 0;
-                    //     // }
-                    //     // else if(c<d){
-                    //     //     return 1;
-                    //     // }
-                    //     // else{
-                    //     //     continue;
-                    //     // }
-                    // }
+                 
                     return two.compareTo(one);
                 }
                 else{
