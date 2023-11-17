79bf7fea15aebb8ff2e206c46604681ff1d1f446
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-11-17 13:27:45 -0600

    Time: 2 ms (83.64%), Space: 41.7 MB (22.35%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,48 +1,54 @@
 class Solution {
-    int[][] DIRECTIONS=new int[][]{{1,0},{-1, 0}, {0, 1}, {0, -1}}; 
+    int m;
+    int n;
+    int[][] DIRECTIONS=new int[][]{ {0,1}, {1,0}, {0,-1}, {-1,0}};
     public int orangesRotting(int[][] grid) {
-        LinkedList<int[]>q=new LinkedList<>();
-        int m=grid.length;
-        int n=grid[0].length;
+        this.m=grid.length;
+        this.n=grid[0].length;
         int oranges=0;
+        Queue<int[]> q=new LinkedList<>();
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j]==2){
-                    q.offer(new int[]{i, j});
-                }
-                if(grid[i][j]==1){
+                if(grid[i][j]==1 ){
                     oranges++;
+                }
+                if(grid[i][j]==2){
+                    q.offer(new int[]{i,j});
                 }
             }
         }
-        q.offer(new int[]{-1, -1}); 
-        int count=0;
+        q.offer(new int[]{-1,-1});
+        int time=0; 
         while(!q.isEmpty()){
-            int curr[]=q.poll(); 
-            int x=curr[0];
-            int y=curr[1];
-            if(x==-1){
+            int[] curr=q.poll();
+            if(curr[0]==-1){
                 if(!q.isEmpty()){
-                    q.offer(new int[]{-1, -1});
-                    count++; 
+                    time++;
+                    q.offer(new int[]{-1,-1});
                 }
                 
             }
-            for(int[] dir: DIRECTIONS){
-                int r=x+dir[0];
-                int c=y+dir[1];
-                if(r< 0 || c < 0 || r==m || c==n || grid[r][c]!=1){
-                    continue; 
+            else{
+                int r=curr[0];
+                int c=curr[1];
+                for(int[] dir: DIRECTIONS){
+                    int x=r+dir[0];
+                    int y=c+dir[1];
+                    if(x>=m || x<0 || y>=n || y<0 || grid[x][y]!=1){
+                        continue;
+                    }
+                    else{
+                        grid[x][y]=2;
+                        oranges--;
+                        q.offer(new int[]{x, y});
+                    }
                 }
-                grid[r][c]=2;
-                oranges--; 
-                q.offer(new int[]{r, c});
             }
         }
         if(oranges>0){
-            return -1; 
+            return -1;
         }
-        System.out.println(oranges);
-        return count; 
+        return time;
+        
     }
 }
