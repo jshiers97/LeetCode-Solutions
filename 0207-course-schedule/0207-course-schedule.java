727c197f1d62074a57e73ca667edaf2de666313c
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-11-18 23:50:58 -0600

    Time: 6 ms (47.87%), Space: 44.4 MB (23.14%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,40 +1,36 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> map=new HashMap<>(); 
         int[] indegree=new int[numCourses];
-        Map<Integer, List<Integer>> map=new HashMap<>();
-        for(int[] prereq: prerequisites){
-            int first=prereq[0];
-            int second=prereq[1];
-            indegree[second]++;
-            if(!map.containsKey(first)){
-                map.put(first, new ArrayList<>()); 
-            }
-            map.get(first).add(second); 
+        for(int i=0; i<numCourses; i++){
+            map.put(i, new ArrayList<>() );
+        }
+        for(int i=0; i<prerequisites.length; i++){
+            int[] curr=prerequisites[i];
+            indegree[curr[0]]++;
+            map.get(curr[1]).add(curr[0]);
+            
         }
         Queue<Integer> q=new LinkedList<>(); 
-        for(int i=0; i<indegree.length; i++){
+        for(int i=0; i<numCourses; i++){
             if(indegree[i]==0){
-                q.offer(i); 
+                q.offer(i);
             }
         }
         while(!q.isEmpty()){
-            int course=q.poll(); 
-            List<Integer> list=map.get(course);
-            if(list==null){
-                continue; 
-            }
-            for(int n: list){
-                indegree[n]--;
-                if(indegree[n]==0){
-                    q.offer(n); 
+            int curr=q.poll(); 
+            for(Integer x: map.get(curr)){
+                indegree[x]--;
+                if(indegree[x]==0){
+                    q.offer(x); 
                 }
             }
         }
         for(int i=0; i<indegree.length; i++){
-            if(indegree[i]>0){
-                return false; 
+            if(indegree[i]!=0){
+                return false;
             }
         }
-        return true; 
+        return true;
     }
 }
