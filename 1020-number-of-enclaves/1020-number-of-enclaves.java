de56fadc7609db8a3060b75489dc891e82f2ee6a
jshiers97 <47252948+jshiers97@users.noreply.github.com>
2023-11-21 00:28:01 -0600

    Time: 16 ms (8.19%), Space: 54.2 MB (68.23%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,115 +1,43 @@
 class Solution {
-    
     public int numEnclaves(int[][] grid) {
-        int number=0;
-        int[][] DIRECTIONS=new int[][]{ {0, 1}, {1,0}, {0, -1}, {-1, 0}};
+        int closed=0; 
+        int[][] DIRECTIONS=new int[][]{ {0,1}, {1, 0}, {-1, 0}, {0, -1}};
+        int total=0;
         int m=grid.length;
         int n=grid[0].length;
-        //top row
-        for(int i=0; i<n; i++){
-            if(grid[0][i]==1){
-                grid[0][i]=0;
-                Queue<int[]> q=new LinkedList<>(); 
-                q.offer(new int[]{0, i});
-                while(!q.isEmpty()){
-                    int[] curr=q.poll(); 
-                    int r=curr[0];
-                    int c=curr[1];
-                    
-                    for(int[] dir: DIRECTIONS){
-                        int x=r+dir[0];
-                        int y=c+dir[1];
-                        if(x>=m || x<0 || y>=n || y<0 || grid[x][y]==0){
-                            continue; 
-                        }
-                        grid[x][y]=0;
-                        q.offer(new int[]{x, y});
-                    }
-                }
-            }
-        }
-        
-        //left col
-        for(int i=0; i<m; i++){
-            if(grid[i][0]==1){
-                grid[i][0]=0;
-                Queue<int[]> q=new LinkedList<>(); 
-                q.offer(new int[]{i,0});
-                while(!q.isEmpty()){
-                    int[] curr=q.poll(); 
-                    int r=curr[0];
-                    int c=curr[1];
-                    
-                    for(int[] dir: DIRECTIONS){
-                        int x=r+dir[0];
-                        int y=c+dir[1];
-                        if(x>=m || x<0 || y>=n || y<0 || grid[x][y]==0){
-                            continue; 
-                        }
-                        grid[x][y]=0;
-                        q.offer(new int[]{x, y});
-                    }
-                }
-            }
-        }
-        
-        //right col
-        for(int i=0; i<m; i++){
-            if(grid[i][n-1]==1){
-                grid[i][n-1]=0;
-                Queue<int[]> q=new LinkedList<>(); 
-                q.offer(new int[]{i, n-1});
-                while(!q.isEmpty()){
-                    int[] curr=q.poll(); 
-                    int r=curr[0];
-                    int c=curr[1];
-                    
-                    for(int[] dir: DIRECTIONS){
-                        int x=r+dir[0];
-                        int y=c+dir[1];
-                        if(x>=m || x<0 || y>=n || y<0 || grid[x][y]==0){
-                            continue; 
-                        }
-                        grid[x][y]=0;
-                        q.offer(new int[]{x, y});
-                    }
-                }
-            }
-        }
-        
-        //bottom row
-        for(int i=0; i<n; i++){
-            if(grid[m-1][i]==1){
-                grid[m-1][i]=0;
-                Queue<int[]> q=new LinkedList<>(); 
-                q.offer(new int[]{m-1, i});
-                while(!q.isEmpty()){
-                    int[] curr=q.poll(); 
-                    int r=curr[0];
-                    int c=curr[1];
-                    
-                    for(int[] dir: DIRECTIONS){
-                        int x=r+dir[0];
-                        int y=c+dir[1];
-                        if(x>=m || x<0 || y>=n || y<0 || grid[x][y]==0){
-                            continue; 
-                        }
-                        grid[x][y]=0;
-                        q.offer(new int[]{x, y});
-                    }
-                }
-            }
-        }
-        
+        //boolean[][] seen=new boolean[m][n];
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j]==1){
-                    number++; 
+                if(grid[i][j]==1 ){
+                    int currNodes=0;
+                    boolean walkOff=false;
+                    Queue<int[]> q=new LinkedList<>(); 
+                    q.offer(new int[]{i, j});
+                    while(!q.isEmpty()){
+                        int[] curr=q.poll(); 
+                        int r=curr[0];
+                        int c=curr[1];
+                        grid[r][c]=-1; 
+                        currNodes++;
+                        if(r==0 || r==m-1 || c==0 || c==n-1){
+                            walkOff=true;
+                        }
+                        for(int[] dir: DIRECTIONS){
+                            int x=r+dir[0];
+                            int y=c+dir[1];
+                            if(x>=0 && x<m && y>=0 && y<n && grid[x][y]==1){
+                                grid[x][y]=-1;
+                                q.offer(new int[]{x, y});
+                                
+                            }
+                        }
+                    }
+                    if(!walkOff){
+                        total+=currNodes;
+                    }
                 }
             }
         }
-        return number; 
-        
-        
+        return total; 
     }
 }
